b755f9c2f906d47751067367058a9ca7c09876ad
ksope <105855785+ksope@users.noreply.github.com>
2023-09-28 14:14:05 +0100

    First commit

---
+++ Dropbox/PC/Documents/CareerFoundry/React-Demo/chat-demo/components/Chat.js
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const Chat = ({ route, navigation }) => {
+    const { name, color } = route.params; //extract the name & color properties passed through the route prop object 
+
+    useEffect(() => {
+        navigation.setOptions({  //update the options configuration for the active screen
+            title: name, //set header title
+            headerStyle: {
+                backgroundColor: color, //set the background color
+            },
+        });
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <Text>Hello Chat!</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+});
+
+export default Chat;
